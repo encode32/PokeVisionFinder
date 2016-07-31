0eab35a4cd52206853ba81dbd396b36ddafae364
encode32 <jesusreto@gmail.com>
2016-07-31 15:14:29 +0200

    Removed Google Api, some changes.

--- PokeVisionFinder.py
+++ PokeVisionFinder.py
@@ -7,36 +7,17 @@ from pokemons import pokemonlist
 
 __author__ = 'encode'
 
-_delay = 1 #seconds
+_delay = 5 #seconds
 
-_pokeVision = False
+_useMode = "Skip" #Skip,Go
 
-_nonstop = True
+_logging = True
 
-_apikey = "" #Your Google Places APIKey
+_zoomFactor = 1 #1 = 0.05 = 13.85Km
 
-_cities = ["London",
-           "NewYork",
-           "Tokyo",
-           "NewDelhi",
-           "WashingtonDC",
-           "Beijing",
-           "Berlin",
-           "Paris",
-           "Moscow",
-           "Mumbai",
-           "Seoul",
-           "Toronto",
-           "Manchester",
-           "Kolkata",
-           "HongKong",
-           "Shanghai",
-           "RioDeJaneiro",
-           "Kiev",
-           "Manila",
-           "Istanbul",
-           "Madrid"
-           ]
+_nonstop = False
+
+_cities = []
 
 _pokemons = []
 
@@ -77,35 +58,60 @@ def _printer(name,lat,lng,exp):
     _remain = exp-time()
     _minutes = int(_remain / 60)
     _seconds = int(_remain % 60)
+    _expire = str(_minutes) + " Minutes, " + str(_seconds) + " Seconds"
     print "-------------------------------------------------"
     print "Pokemon: " + name
     print "Coordinates: " + str(lat) + "," + str(lng)
-    print "Expires in: " + str(_minutes) + " Minutes, " + str(_seconds) + " Seconds."
+    print "Expires in: " + _expire
     print "-------------------------------------------------"
+    _logPokemon(name, str(lat), str(lng), _expire)
+
+#CoordsLoader
+def _populateCities():
+    with open("coords.txt", "a+") as f:
+        _data = f.readlines()
+        for line in _data:
+            _citydata = line.split(":")
+            _cities.append([_citydata[0],_citydata[1],_citydata[2]])
+        f.close()
+
+#Logger
+def _logPokemon(name, lat, lng, expire):
+    with open("pokemons.log", "a+") as f:
+        f.write("[" + name + "] [" + lat + "," + lng + "] [" + expire + "]\n")
+        f.close()
+
+#Finder
+def _finderSkipLagged(city):
+    print "[INFO] Looking pokemons in: " + city[0]
+    _latitudesw = float(city[1]) - (0.05 * _zoomFactor)
+    _longitudesw = float(city[2]) - (0.05 * _zoomFactor)
+    _latitudene = float(city[1]) + (0.05 * _zoomFactor)
+    _longitudene = float(city[2]) + (0.05 * _zoomFactor)
+
+    _scanurl = "https://skiplagged.com/api/pokemon.php?bounds="+str(_latitudesw)+","+str(_longitudesw)+\
+               ","+str(_latitudene)+","+str(_longitudene)
+    _scanurljsondata = _jsondatach(_scanurl)
+
+    print _scanurl
+
+    for pokename in _pokemons:
+        for pokemon in _scanurljsondata['pokemons']:
+            _id = pokemon['pokemon_id']
+            _name = _pokename(_id)
+            if pokename.lower() in _name.lower():
+                _lat = pokemon['latitude']
+                _lng = pokemon['longitude']
+                _exp = pokemon['expires']
+                _printer(_name, _lat, _lng, _exp)
 
 #Finder
 def _finderGo(city):
     print "[INFO] Looking pokemons in: " + city
-    _googleacurl = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input="+\
-               city+"&types=(cities)&key="+_apikey
-    _googleacjsondata = _jsondata(_googleacurl)
-
-    _cityreference = _googleacjsondata['predictions'][0]['reference']
-
-    _googledurl = "https://maps.googleapis.com/maps/api/place/details/json?reference="+\
-              _cityreference+"&sensor=true&key="+_apikey
-    _googledjsondata = _jsondata(_googledurl)
-
-    _latitude = _googledjsondata['result']['geometry']['location']['lat']
-    _longitude = _googledjsondata['result']['geometry']['location']['lng']
-
-    _latitudene = _googledjsondata['result']['geometry']['viewport']['northeast']['lat']
-    _longitudene = _googledjsondata['result']['geometry']['viewport']['northeast']['lng']
-
-    _latitudesw = _googledjsondata['result']['geometry']['viewport']['southwest']['lat']
-    _longitudesw = _googledjsondata['result']['geometry']['viewport']['southwest']['lng']
-
-    _statusWait = True
+    _latitudesw = float(city[1]) - (0.05 * _zoomFactor)
+    _longitudesw = float(city[1]) - (0.05 * _zoomFactor)
+    _latitudene = float(city[2]) + (0.05 * _zoomFactor)
+    _longitudene = float(city[2]) + (0.05 * _zoomFactor)
 
     _scanurl = "https://api-live-us1.pokemongo.id/maps?vt=-"+str(_latitudesw)+","+str(_longitudesw)+\
                ","+str(_latitudene)+","+str(_longitudene)+"&u="+str(time())
@@ -121,82 +127,32 @@ def _finderGo(city):
                 _exp = pokemon['expires']
                 _printer(_name, _lat, _lng, _exp)
 
-#Finder
-def _finderPokeVision(city):
-    print "[INFO] Looking pokemons in: " + city
-    _googleacurl = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input="+\
-               city+"&types=(cities)&key="+_apikey
-    _googleacjsondata = _jsondata(_googleacurl)
-
-    _cityreference = _googleacjsondata['predictions'][0]['reference']
-
-    _googledurl = "https://maps.googleapis.com/maps/api/place/details/json?reference="+\
-              _cityreference+"&sensor=true&key="+_apikey
-    _googledjsondata = _jsondata(_googledurl)
-
-    _latitude = _googledjsondata['result']['geometry']['location']['lat']
-    _longitude = _googledjsondata['result']['geometry']['location']['lng']
-
-    _latitudene = _googledjsondata['result']['geometry']['viewport']['northeast']['lat']
-    _longitudene = _googledjsondata['result']['geometry']['viewport']['northeast']['lng']
-
-    _latitudesw = _googledjsondata['result']['geometry']['viewport']['southwest']['lat']
-    _longitudesw = _googledjsondata['result']['geometry']['viewport']['southwest']['lng']
-
-    _coords = [[_latitude,_longitude],
-        [_latitudene,_longitudene],
-        [_latitudesw,_longitudesw]]
-    for _coord in _coords:
-
-        _statusWait = True
-
-        _scanurl = "https://pokevision.com/map/scan/"+str(_coord[0])+"/"+str(_coord[1])
-        _scanurljsondata = {}
-
-        while _statusWait:
-            _scanurljsondata = _jsondatach(_scanurl)
-            _status = _scanurljsondata['status']
-            _sleep()
-            if 'success' in _status: _statusWait = False
-        _jobid = _scanurljsondata['jobId']
-
-        _jobStatus = True
-
-        _dataurl = "https://pokevision.com/map/data/"+str(_coord[0])+"/"+str(_coord[1])+"/"+_jobid
-        _dataurljsondata = {}
-
-        while _jobStatus:
-            _dataurljsondata = _jsondatach(_dataurl)
-            _sleep()
-            if 'jobStatus' not in _dataurljsondata: _jobStatus = False
-
-        for pokename in _pokemons:
-            for pokemon in _dataurljsondata['pokemon']:
-                _id = pokemon['pokemonId']
-                _name = _pokename(_id)
-                if pokename.lower() in _name.lower():
-                    _lat = pokemon['latitude']
-                    _lng = pokemon['longitude']
-                    _exp = pokemon['expiration_time']
-                    _printer(_name, _lat, _lng, _exp)
 
 #Loop
 def _loop():
     for city in _cities:
-        if _pokeVision:
-            _finderPokeVision(city)
-        else:
+        if "Skip" in _useMode:
+            _finderSkipLagged(city)
+        elif "Go" in _useMode:
             _finderGo(city)
 
 #Init
 _inputpoke = ""
-if len(argv) > 1:
+
+_populateCities()
+if len(argv) == 2:
     _inputpoke = argv[1]
+elif len(argv) == 5:
+    _inputpoke = argv[1]
+    _nonstop = int(argv[2]) == 1
+    _zoomFactor = float(argv[3])
+    _logging = int(argv[4]) == 1
 else:
     _inputpoke = raw_input("Pokemon: ")
 _pokesplit(_inputpoke)
 if _nonstop:
     while 1:
         _loop()
+        _sleep()
 else:
     _loop()
